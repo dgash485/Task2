91479dbe23de7427df340fa483e31db6b8964153
dgash485 <59995121+dgash485@users.noreply.github.com>
2020-10-05 21:47:33 +0300

    Add files via upload

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Task2
+{
+    interface IRadio
+    {
+        void Play();
+    }
+
+    interface IAlarmClock
+    {
+        void Ring(DateTime time);
+    }
+
+    class Radio : IRadio
+    {
+        public Radio() {} 
+
+        public void Play()
+        {
+            Console.WriteLine("Радио играет!");
+        }
+    }
+
+    class AlarmClock : IAlarmClock
+    {
+        private DateTime _ringTime;
+
+        public AlarmClock(DateTime ringTime)
+        {
+            _ringTime = ringTime;
+        }
+
+        public void Ring(DateTime time)
+        {
+            if (_ringTime.Equals(time))
+            {
+                Console.WriteLine("Будильник звонит!");
+            }
+        }
+    }
+
+    class RadioAlarmClock : IRadio, IAlarmClock
+    {
+        private DateTime _ringTime;
+
+        public RadioAlarmClock(DateTime ringTime)
+        {
+            _ringTime = ringTime;
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Радиобудульник играет!");
+        }
+
+        public void Ring(DateTime time)
+        {
+            if(_ringTime.Equals(time));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
